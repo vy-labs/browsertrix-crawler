e202513a2269cf57a0ff518ac8ad5c1360065feb
Lakshya <lbansal123123@gmail.com>
2023-06-23 18:03:48 +0530

    added retry and common data to logs

--- util/broadCrawlRedis.js
+++ util/broadCrawlRedis.js
@@ -3,7 +3,9 @@ import Redis from "ioredis";
 export async function initBroadCrawlRedis() {
   const board_crawl_redis = new Redis({
     host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD,
+    lazyConnect: true
   });
   await board_crawl_redis.connect();
   return board_crawl_redis;
